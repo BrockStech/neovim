3fddcd92d2973c2c766d2a1ac66eda61ccba4125
Brock Stechschulte <brockstechschulte@Brocks-Air.home>
2024-06-11 13:52:26 -0400

    add vim-test

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,3 +1,10 @@
 return {
+  dependencies = {
+    'preservim/vimux',
+  },
   'vim-test/vim-test',
+  vim.keymap.set('n', '<leader>tt', ':TestNearest<CR>'),
+  vim.keymap.set('n', '<leader>tf', ':TestFile<CR>'),
+  vim.keymap.set('n', '<leader>tl', ':TestLast<CR>'),
+  vim.cmd "let test#strategy = 'vimux'",
 }
